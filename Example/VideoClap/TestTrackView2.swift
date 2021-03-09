83024a68a67d00c12487b8ec2ca2e9eab022078c
John Pope <john@8secondz.app>
2021-03-09 18:42:01 +1100

    WARNING - bump target to ios 13. on load video + play chase the scrollview.

--- Example/VideoClap/TestTrackView2.swift
+++ Example/VideoClap/TestTrackView2.swift
@@ -63,6 +63,8 @@ class TestTrackView2: UIViewController {
         return label
     }()
     
+    let progressBar = NowPlayingProgressBar()
+    
     lazy var playButton: UIButton = {
         let button = UIButton()
         button.layer.cornerRadius = 8
@@ -221,9 +223,9 @@ class TestTrackView2: UIViewController {
         videoDescription.fps = 24.0
         videoDescription.renderScale = 1
         videoDescription.renderSize = KResolution720x1280
+        addPeriodicTimeObserver()
         
         
-
     }
     
     func attemptToRefreshScrollview(){
@@ -265,6 +267,13 @@ class TestTrackView2: UIViewController {
         view.addSubview(slider)
         view.addSubview(playButton)
         view.addSubview(timelabel)
+        view.addSubview(progressBar)
+        progressBar.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(2)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
         containerView.snp.makeConstraints { (make) in
             make.top.equalToSuperview().offset(2)
             make.left.right.equalToSuperview()
@@ -425,6 +434,28 @@ class TestTrackView2: UIViewController {
             }
         }
     }
+    var timeObserverToken: Any?
+    
+    func addPeriodicTimeObserver() {
+     
+        timeObserverToken = self.player.addPeriodicTimeObserver(forInterval: CMTime(value: 1, timescale: 30), queue: DispatchQueue.main) { (time) in
+
+            //if self.isDragging {return}
+            let playTime = Float(CMTimeGetSeconds(time))
+            let totalDuration =  self.player.currentItem?.asset.duration.seconds ?? 0.000001
+            //self.timeLabel.text = self.strWithTime(time: playTime)
+            let x = CGFloat(playTime/Float(totalDuration)) * self.mainTrackView.collectionView.contentSize.width - self.scrollView.contentInset.left
+            self.scrollView.setContentOffset(CGPoint(x: x, y: 0), animated: false)
+
+        }
+    }
+    
+    func removePeriodicTimeObserver() {
+        if let timeObserverToken = timeObserverToken {
+            player.removeTimeObserver(timeObserverToken)
+            self.timeObserverToken = nil
+        }
+    }
     
     @objc func timer() {
         let currentTime: CMTime = player.currentItem?.currentTime() ?? .zero
@@ -449,10 +480,7 @@ class TestTrackView2: UIViewController {
             self.playButton.isSelected = isSelected
             self.slider.value = value
             
-            let percentage = (self.scrollView.contentOffset.x + self.scrollView.contentInset.left) / (self.scrollView.contentSize.width)
-            print("current percent:",percentage)
-            let offset = CGPoint(x:self.scrollView.contentSize.width * CGFloat(value) + self.scrollView.contentInset.left, y:self.scrollView.contentOffset.y)
-            self.scrollView.setContentOffset(offset, animated: true)
+
         }
     }
     
@@ -616,4 +644,148 @@ extension TestTrackView2: (UIImagePickerControllerDelegate & UINavigationControl
         picker.dismiss(animated: true, completion: nil)
     }
     
+    var nowPlayingProgress: NowPlayingProgress {
+        return NowPlayingProgress(
+            elapsedTime: player.currentTime().seconds,
+            duration: player.currentItem?.asset.duration.seconds ?? 0.0
+        )
+    }
+}
+
+import UIKit
+
+struct NowPlayingProgress {
+    var elapsedTime: Double = 0.0
+    var duration: Double = 0.0
+}
+
+protocol NowPlayingProgressBarDelegate : class {
+    func progressBarDidChangeValue(
+        to time: TimeInterval, progressBar: NowPlayingProgressBar, completion: @escaping () -> Void
+    )
+}
+
+class NowPlayingProgressBar : UIView {
+    private static let thumbImage = UIImage(systemName: "circle.fill")!
+    private let slider = UISlider()
+    private let elapsedLabel = UILabel()
+    private let remainingLabel = UILabel()
+    private let adjustingSliderThumb = UIImageView(image: NowPlayingProgressBar.thumbImage)
+    private var isAdjustingSlider = false {
+        didSet { animateAdjustingThumb() }
+    }
+    
+    weak var delegate: NowPlayingProgressBarDelegate? {
+        didSet { slider.isEnabled = delegate != nil }
+    }
+    
+    var elapsedTime: TimeInterval {
+        get { progress.elapsedTime }
+        set { progress.elapsedTime = newValue }
+    }
+    
+    var progress = NowPlayingProgress() {
+        didSet { updateProgressBar() }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+       // elapsedLabel.font = .preferredFont(forTextStyle: .callout, design: .rounded)
+        elapsedLabel.textColor = .secondaryLabel
+        elapsedLabel.textAlignment = .left
+        elapsedLabel.text = "0:00"
+        
+       // remainingLabel.font = .preferredFont(forTextStyle: .callout, design: .rounded)
+        remainingLabel.textColor = .secondaryLabel
+        remainingLabel.textAlignment = .right
+        remainingLabel.text = "-0:00"
+        
+        let config = UIImage.SymbolConfiguration(textStyle: .footnote, scale: .small)
+        slider.setThumbImage(NowPlayingProgressBar.thumbImage.withConfiguration(config), for: .normal)
+        slider.addTarget(self, action: #selector(sliderTouchDown), for: .touchDown)
+        slider.addTarget(self, action: #selector(sliderTouchUp), for: [.touchUpInside, .touchUpOutside])
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+        
+        adjustingSliderThumb.alpha = 0
+        adjustingSliderThumb.transform = .minimal
+        adjustingSliderThumb.preferredSymbolConfiguration = .init(textStyle: .largeTitle, scale: .large)
+        addSubview(adjustingSliderThumb)
+        
+        let labels = UIStackView(arrangedSubviews: [elapsedLabel, remainingLabel])
+        labels.axis = .horizontal
+        labels.distribution = .fillEqually
+        
+        let contentView = UIStackView(arrangedSubviews: [slider, labels])
+        contentView.axis = .vertical
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentView.topAnchor.constraint(equalTo: topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            heightAnchor.constraint(greaterThanOrEqualToConstant: 44)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
+    
+    private func updateProgressBar() {
+        slider.minimumValue = 0.0
+        slider.maximumValue = Float(progress.duration)
+        guard !isAdjustingSlider else { return }
+        updateLabels(with: progress.elapsedTime)
+        slider.setValue(Float(progress.elapsedTime), animated: true)
+        updateAdjustingThumbPosition()
+    }
+    
+    private func updateLabels(with time: TimeInterval) {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.minute, .second]
+        formatter.zeroFormattingBehavior = .pad
+        elapsedLabel.text = formatter.string(from: time.isNaN ? 0.0 : time)
+        remainingLabel.text = formatter.string(from: progress.duration)
+    }
+    
+    @objc private func sliderTouchDown() {
+        isAdjustingSlider = true
+    }
+    
+    @objc private func sliderTouchUp() {
+        delegate?.progressBarDidChangeValue(to: Double(slider.value), progressBar: self) {
+            self.isAdjustingSlider = false
+        }
+    }
+    
+    @objc private func sliderValueChanged() {
+        updateAdjustingThumbPosition()
+        updateLabels(with: Double(slider.value))
+    }
+    
+    private func animateAdjustingThumb() {
+        let targetAlpha: CGFloat = isAdjustingSlider ? 1.0 : 0.0
+        let transform = isAdjustingSlider ? CGAffineTransform.fullSize : .minimal
+        UIView.animate(
+            withDuration: 0.8, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8,
+            options: .beginFromCurrentState, animations: {
+                self.adjustingSliderThumb.transform = transform
+                self.adjustingSliderThumb.alpha = targetAlpha
+            }, completion: nil)
+    }
+    
+    private func updateAdjustingThumbPosition() {
+        let fractionalLocation = CGFloat(slider.value / (slider.maximumValue - slider.minimumValue))
+        let thumbWidth = slider.currentThumbImage!.size.width
+        let maximumWidth = slider.frame.width - thumbWidth
+        let positionInSlider = CGPoint(x: thumbWidth/2 + fractionalLocation * maximumWidth, y: slider.frame.height / 2)
+        adjustingSliderThumb.center = slider.convert(positionInSlider, to: self)
+    }
+}
+
+private extension CGAffineTransform {
+    static let minimal = CGAffineTransform(scaleX: 0.25, y: 0.25)
+    static let fullSize = CGAffineTransform(scaleX: 1.25, y: 1.25)
 }
